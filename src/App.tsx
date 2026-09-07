1d468b9a4c98fd08019eff94e1b09dac332b005e
luvsickangelx19-max <luvsickangelx19@gmail.com>
2026-09-07 20:26:15 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -193,7 +193,7 @@ function App() {
               </p>
               <p style={{ fontSize: '0.95em', color: '#777' }}>
                  Periksa apakah daerah rumah Anda masuk ke dalam jangkaun layanan kami pada daftar di bawah ini. Cukup pesan lewat WhatsApp, tim kami yang akan datang membawa seluruh peralatan lengkap!
-                </p>
+               </p>
               </div>
           </div>
         </section>
